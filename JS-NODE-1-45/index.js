12628347a96d143b263993b6c0c01d7a7f5082f5
Asadkarim <asadkarim33@gmail.com>
2022-11-10 21:39:47 +0500

    js task done 1 to 13

---
+++ JS-NODE-1-45/index.js
@@ -0,0 +1,111 @@
+//Task 1 
+//node js installed
+
+
+//Task 2 
+
+//var Asad = "Hello Eric, would you like to learn some Python today?"
+//console.log(Asad)
+
+//Task 3 
+
+// var Asad = "Asad"
+
+// var Asad1 = Asad.toLowerCase();
+// var Asad2 = Asad.toUpperCase();
+
+// console.log(Asad1)
+// console.log(Asad2)
+
+// const str = "asad karim mughal";
+// const newStr = str.split(' ')
+//    .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
+//    .join(' ');
+// console.log(newStr);
+
+// Task 4
+
+// let Asad = "Mohd Ali jinnah once said, “I do not believe in taking the right decision, I take a decision and make it right”"
+
+// console.log(Asad)
+
+// Task 5
+
+    // let famous_person = "Mohd Ali said"
+    // let message = " “ Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”"
+
+    // console.log(famous_person + message)
+
+
+ // Task 6
+ 
+//  let Name = "      asad \n karim \t mughal           "
+//  let NameN = "      asad \n karim \t mughal           ".replace(/\s+/g, ' ').trim()
+
+
+//  console.log(Name)
+//  console.log(NameN)
+
+// Task 7
+
+// var eight = 8 * 1
+// var eight1 = 8 + 2
+// var eight2 = 8 - 4
+// var eight3 = 8 % 3
+
+// console.log(eight3)
+// console.log(eight2)
+// console.log(eight1)
+// console.log(eight) 
+
+// Task 8
+
+
+// console.log(5 + 3)
+// console.log(5 + 3)
+// console.log(5 + 3)
+// console.log(5 + 3)
+
+// Task 9
+
+
+// var fav_num = 42
+// var msg = "My favorite number is " + fav_num + "."
+
+// console.log(msg)
+
+
+//Task 11
+// const friends = ["Asad", "imran", "muneez","waqar"];
+
+// console.log(friends[0])
+// console.log(friends[0])
+// console.log(friends[0])
+
+//Task 12
+
+// let names = ['Asad', 'imran', 'muneez','waqar']
+
+// var msg1 = "Hello, " + names[0] + "!"
+// console.log(msg1)
+
+// let msg2 = "Hello, " + names[1]  + "!"
+// console.log(msg2)
+
+// const msg3 = "Hello, " + names[2] + "!"
+// console.log(msg3)
+
+// const msg4 = "Hello, " + names[3] + "!"
+// console.log(msg4)
+
+//Task 13
+
+// var favorite_transportation = ['car', "airplane","bike"]
+
+// console.log("I go to work using " + ( favorite_transportation[0]))
+// console.log("I travel overseas using " +(favorite_transportation[1]))
+// console.log("I love to ride on " +(favorite_transportation[2]))
+
+
+//Task 14
+
